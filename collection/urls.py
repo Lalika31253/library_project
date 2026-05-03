7925e1e8e78b71a30e9a693031e766914ed57a39
Alina <Lalika31253@gmail.com>
2026-05-03 15:34:50 -0600

    Update urls file

---
+++ collection/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Locations
+    path('',                 views.LocationListView.as_view(),  name='location-list'),
+    path('location/<int:pk>/', views.LocationDetailView.as_view(), name='location-detail'),
+
+    # Books
+    path('books/',                    views.BookListView.as_view(),       name='book-list'),
+    path('book/add/',                views.BookCreateView.as_view(),     name='book-create'),
+    path('books/<int:pk>/edit/',      views.BookUpdateView.as_view(),     name='book-update'),
+    path('books/<int:pk>/delete/',    views.BookDeleteView.as_view(),     name='book-delete'),
+    path('books/<int:pk>/inline-delete/',
+                                     views.BookInlineDeleteView.as_view(),
+                                     name='book-inline-delete'),
+    # HTMX search endpoint
+    path('books/search/',             views.BookSearchView.as_view(),     name='book-search'),
+
+    # Visitors
+    path('visitors/',                 views.VisitorListView.as_view(),    name='visitor-list'),
+    path('visitors/<int:pk>/',        views.VisitorDetailView.as_view(),  name='visitor-detail'),
+    path('visitors/add/',             views.VisitorCreateView.as_view(),  name='visitor-create'),
+    path('visitors/<int:pk>/edit/',   views.VisitorUpdateView.as_view(),  name='visitor-update'),
+    path('visitors/<int:pk>/delete/', views.VisitorDeleteView.as_view(),  name='visitor-delete'),
+]
